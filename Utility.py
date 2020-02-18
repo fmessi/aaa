4bbb11e841744410901e2482c7931edfff77f596
FraMe <francesco.messi@nuclear.lu.se>
2020-02-18 11:35:05 +0100

    added functions to calculate the irradiation from a given source at a given distance 	modified:   Utility.py

--- Utility.py
+++ Utility.py
@@ -13,6 +13,7 @@
 '''
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 def Plot1D(dataArray, nBin=0, R=(0,0), title='title here...', xlabel='here label...', label='data', c=1, log=False, weights=None):
     '''
@@ -32,3 +33,28 @@ def Plot1D(dataArray, nBin=0, R=(0,0), title='title here...', xlabel='here label
     n, bins, patches = plt.hist(dataArray, bins=nBin, range=R,  histtype='step', fill=False, log=log, label=label, weights=weights)
     #return dataArray.hist(bins=nBin, histtype='step', fill=False ) #,edgecolor='black')
     return n, bins, patches
+
+def IrradiationRate(A = 2500, D = 1500, R = 2.99E6):
+    '''
+    calculate the irradiation rate from the setup
+    '''
+    #A = 2500 ## active area of the detector (in mmˆ2)
+    #D = 1500 ## distance from the source (in mm)
+    #R = 2.99E6  ## emission rate of the source (in n/sec)
+    ## PuBe => R = 2.99E6 n/sec  // D = 130+acquarium
+    ## AmBe => R = 1.14E6 n/sec  // D = 255 mm
+    ## Co60 => R =   // D = 110 mm
+    sigma = A / (4 * np.pi * D * D) * R
+    return(sigma)
+
+def CalculateIrradiation(A = 2500, D = 1500, Rmin=100, Rmax=2500,
+                         R = 2.99E6, label='source'):
+    Ir = []
+    d = []
+    for a in range(Rmin,Rmax):
+        Ir.append(IrradiationRate(A=A, D=a, R=R))
+        d.append(a)
+    plt.plot(d,Ir, label=label)
+    plt.xlabel('distance (mm)')
+    plt.ylabel('irradiation (n/sec)')
+    plt.legend()
